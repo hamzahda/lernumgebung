d617cac09c440dd02955804b6390aee543194896
Hamza <hamzahda@outlook.com>
2020-05-13 19:23:57 +0200

    added docker compose and mysql init,  doesnt work yet

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,7 +10,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'mysql_db',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'root',
